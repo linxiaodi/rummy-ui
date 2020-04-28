82312fba97a99e748b8a533ffc4c25e7036ac545
linweikun <linweikun@souche.com>
2020-04-28 17:35:30 +0800

    feat:给dialog加上动画

--- lib/dialog/index.tsx
+++ lib/dialog/index.tsx
@@ -1,103 +1,101 @@
 // @ts-ignore
-import React, { Fragment, ReactElement } from 'react';
+import React, { Fragment, ReactElement, useState } from 'react';
 import ReactDOM from 'react-dom';
 import cs from 'classnames';
 import { setBodyOverflow } from '../_util/dom';
 import { Button, Icon } from '..';
 import './index.scss';
 import { scopeHelper } from '../_util/helpers';
+import Transition from '../transition/index';
 
 const scope = scopeHelper('ru-dialog');
 
 interface DialogProps {
-  visible: Boolean,
-  title?: string | React.ReactNode,
-  className?: string,
-  children?: string | React.ReactNode,
-  onCancel?: () => any,
-  buttons?: React.ReactNode | Array<React.ReactElement>,
-  closeOnClickMask?: boolean,
+	visible: boolean;
+	title?: string | React.ReactNode;
+	className?: string;
+	children?: string | React.ReactNode;
+	onCancel?: () => any;
+	buttons?: React.ReactNode | Array<React.ReactElement>;
+	closeOnClickMask?: boolean;
+	afterClose?: () => any;
 }
 
 interface Species {
-  content: React.ReactNode,
-  title: React.ReactNode,
-  afterClose?: () => void,
+	content: React.ReactNode;
+	title: React.ReactNode;
+	afterClose?: () => void;
 }
 
 interface ModalProps extends Species {
-  buttons?: Array<React.ReactElement>
+	buttons?: Array<React.ReactElement>;
 }
 
 interface AlertProps extends Species {}
 
 interface ConfirmProps extends Species {
-  onOk?: () => void
+	onOk?: () => void;
 }
 
 interface DialogType extends React.FunctionComponent<DialogProps> {
-  alert?: (props: AlertProps) => any
+	alert?: (props: AlertProps) => any;
 }
 
-const Dialog: DialogType = (props) => {
-  const { title, visible, buttons, closeOnClickMask } = props;
-  const wrapperClass = cs(props.className, scope('mask'), { 'ru-hide': !visible });
-  const dialogClass = cs({ 'ru-hide': !visible }, scope());
-  if (visible) {
-    setBodyOverflow('hidden');
-  } else {
-    setBodyOverflow();
-  }
-  const onCancel = () => props.onCancel && props.onCancel();
-  // footer
-  let dialogFooter;
-  if (Array.isArray(buttons)) {
-    dialogFooter = (buttons as Array<ReactElement>).map((Comp, i) => {
-      // 重新复制一份，相当于一个简单的高阶函数
-      return React.cloneElement(Comp, { key: i });
-    });
-  } else {
-    if (buttons) {
-      dialogFooter = buttons;
-    }
-  }
+const Dialog: DialogType = props => {
+	const { title, visible, buttons, closeOnClickMask } = props;
+	const wrapperClass = cs(props.className, scope('mask'), { 'ru-hide': !visible });
+	const dialogClass = cs(scope());
+	if (visible) {
+		setBodyOverflow('hidden');
+	} else {
+		setBodyOverflow();
+	}
+	const onCancel = () => props.onCancel && props.onCancel();
+	// footer
+	let dialogFooter;
+	if (Array.isArray(buttons)) {
+		dialogFooter = (buttons as Array<ReactElement>).map((Comp, i) => {
+			// 重新复制一份，相当于一个简单的高阶函数
+			return React.cloneElement(Comp, { key: i });
+		});
+	} else {
+		if (buttons) {
+			dialogFooter = buttons;
+		}
+	}
 
-  // 点击mask
-  const onMaskClick: React.MouseEventHandler = () => {
-    if (closeOnClickMask) {
-      onCancel();
-    }
-  };
+	// 点击mask
+	const onMaskClick: React.MouseEventHandler = () => {
+		if (closeOnClickMask) {
+			onCancel();
+		}
+	};
 
-  // 点击esc
-  // const onEscKeyDown:React.KeyboardEventHandler = (e) => {
-  //   console.log(e.keyCode);
-  //   if (e.keyCode === 27 && props.keyboard) {
-  //     onCancel();
-  //   }
-  // }
+	// 点击esc
+	// const onEscKeyDown:React.KeyboardEventHandler = (e) => {
+	//   console.log(e.keyCode);
+	//   if (e.keyCode === 27 && props.keyboard) {
+	//     onCancel();
+	//   }
+	// }
 
-  const result = (
-    <Fragment>
-      <div onClick={onMaskClick} className={wrapperClass}/>
-        <div className={dialogClass}>
-          <div className={scope('header')}>
-            <div className={scope('header__title')}>{title}</div>
-            <Icon onClick={onCancel} className={scope('close')} name="close"/>
-          </div>
-          <div className={scope('body')}>
-            {props.children}
-          </div>
-          {
-            dialogFooter && <div className={scope('footer')}>
-              {dialogFooter}
-            </div>
-          }
-        </div>
-    </Fragment>
-  );
-  // 把dialog放在最外层
-  return ReactDOM.createPortal(result, document.body);
+	const result = (
+		<div>
+			<div onClick={onMaskClick} className={wrapperClass} />
+			<Transition onExited={props.afterClose} in={visible} classNames="ru-zoom">
+				<div className={dialogClass}>
+					<div className={scope('header')}>
+						<div className={scope('header__title')}>{title}</div>
+						<Icon onClick={onCancel} className={scope('close')} name="close" />
+					</div>
+					<div className={scope('body')}>{props.children}</div>
+					{dialogFooter && <div className={scope('footer')}>{dialogFooter}</div>}
+				</div>
+			</Transition>
+		</div>
+	);
+	// 把dialog放在最外层
+	return ReactDOM.createPortal(result, document.body);
 };
 
 // export const alert = () => {
@@ -108,57 +106,76 @@ const Dialog: DialogType = (props) => {
  * */
 
 export const modal = (props: ModalProps) => {
-  const { title, afterClose, buttons, content } = props;
-  const close = () => {
-    ReactDOM.render(React.cloneElement(component, { visible: false }), wrapper);
-    ReactDOM.unmountComponentAtNode(wrapper);
-    wrapper.remove();
-  }
+	const { title, afterClose, buttons, content } = props;
+	const close = () => {
+		ReactDOM.render(
+			React.cloneElement(component, {
+				visible: false,
+				afterClose: () => {
+					// 动画结束之后删除实例
+					ReactDOM.unmountComponentAtNode(wrapper);
+					wrapper.remove();
+				}
+			}),
+			wrapper
+		);
+	};
 
-  const onCancel  = () => {
-    afterClose && afterClose();
-    close();
-  };
+	const onCancel = () => {
+		afterClose && afterClose();
+		close();
+	};
 
-  const component = (<Dialog visible={true} buttons={buttons} onCancel={onCancel } title={title}>
-    {content}
-  </Dialog>);
+	const component = (
+		<Dialog visible={true} buttons={buttons} onCancel={onCancel} title={title}>
+			{content}
+		</Dialog>
+	);
 
-  const wrapper = document.createElement('div');
+	const wrapper = document.createElement('div');
 
-  ReactDOM.render(component, wrapper)
-  document.body.append(wrapper);
-  return close;
+	ReactDOM.render(component, wrapper);
+	document.body.append(wrapper);
+	return close;
 };
 
 export const alert = (props: AlertProps) => {
-  const closeBtn = <Button onClick={() => close()}>关闭</Button>
-  const close = modal({
-    ...props,
-    buttons: [closeBtn]
-  })
-}
+	const closeBtn = <Button onClick={() => close()}>关闭</Button>;
+	const close = modal({
+		...props,
+		buttons: [closeBtn]
+	});
+};
 
 export const confirm = (props: ConfirmProps) => {
-  const { afterClose, onOk } = props;
-  const btns = [<Button onClick={() => {
-    close();
-    afterClose && afterClose();
-  }}>取消</Button>, <Button type="primary" onClick={() => {
-    close();
-    onOk && onOk();
-  }}>确定</Button>]
-  const close = modal({ ...props, buttons: btns });
-}
+	const { afterClose, onOk } = props;
+	const btns = [
+		<Button
+			onClick={() => {
+				close();
+				afterClose && afterClose();
+			}}>
+			取消
+		</Button>,
+		<Button
+			type="primary"
+			onClick={() => {
+				close();
+				onOk && onOk();
+			}}>
+			确定
+		</Button>
+	];
+	const close = modal({ ...props, buttons: btns });
+};
 
 // Dialog.propTypes = {
 // }
 
 Dialog.defaultProps = {
-  closeOnClickMask: true
+	closeOnClickMask: true
 };
 
 Dialog.alert = alert;
-
 
 export default Dialog;
--- lib/transition/Transition.example.tsx
+++ lib/transition/Transition.example.tsx
@@ -1,34 +1,37 @@
 import React, { useState } from 'react';
 import Demo from '../../example/demo';
-import Table from '../../example/table'
+import Table from '../../example/table';
 
 const App = () => {
-  return (
-    <div>
-      <h2>Transition 过渡动画</h2>
-      <p>内置的过渡动画组件，只支持CSS属性的过渡动画。</p>
-      <Demo
-        title="基础使用"
-        desc="绑定一个字符串classNames，相当于绑定了一系列的className，比如fade，相当于声明{
+	return (
+		<div>
+			<h2>Transition 过渡动画</h2>
+			<p>内置的过渡动画组件，只支持CSS属性的过渡动画。</p>
+			<Demo
+				title="基础使用"
+				desc="绑定一个字符串classNames，相当于绑定了一系列的className，比如fade，相当于声明{
           enter: fade-enter, enterActive: fade-enter-active, enterDone: fade-enter-done,
           exit: fade-exit, exitActive: fade-exit-active, exitDone: fade-exit-done
         }"
-        path="transition/examples/basic"/>
-      <Demo
-        path="transition/examples/custom"/>
+				path="transition/examples/basic"
+			/>
+			<Demo path="transition/examples/custom" />
 
-      <h3>API</h3>
-      <Table
-        title="Transition Attributes"
-        type="attribute"
-        source={[
-          ['in', '控制组件显示隐藏', 'boolean', '必填', '-'],
-          ['classNames', '控制组件的CSS动画，如果声明的是string,会默认为：一系列的classNames', 'string | object', '必填', '-'],
-          ['onEnter', '当组件开始动画时', '() => any', '非必填', 'any'],
-        ]}
-      />
-    </div>
-  )
-}
+			<h3>API</h3>
+			<Table
+				title="Transition Attributes"
+				type="attribute"
+				source={[
+					['in', '控制组件显示隐藏', 'boolean', '必填', '-'],
+					['classNames', '控制组件的CSS动画，如果声明的是string,会默认为：一系列的classNames', 'string | object', '必填', '-'],
+					['onEnter', '当组件开始动画时', '() => any', '非必填', 'any'],
+					['onEntered', '当组件动画完成之后', '() => any', '非必填', 'any'],
+					['onExit', '当组件执行离开动画时', '() => any', '非必填', 'any'],
+					['onExited', '当组件动画完成离开动画之后', '() => any', '非必填', 'any']
+				]}
+			/>
+		</div>
+	);
+};
 
-export default App
+export default App;
